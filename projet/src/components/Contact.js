ca64c7219c11a30cdefa2d781e35966c86a6de9c
Roxane <roxane.batigny@edu.ece.fr>
2019-04-04 19:56:37 +0200

    ajout 2 forms en plus pas réussi

---
+++ projet/src/components/Contact.js
@@ -0,0 +1,166 @@
+import React, { Component } from 'react';
+
+
+
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+import './Contact.css';
+
+export default class Example extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  this.state = {value: ''};
+  this.handleChange = this.handleChange.bind(this);
+  this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Un nouvel artiste a été ajouté');
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <Form>
+
+      <Row form>
+
+      <Col md="4">
+
+
+     
+      <Form className = "NewArtist" onSubmit={this.handleSubmit}>
+
+      <h1>Ajouter un Artiste</h1>
+
+      
+        <FormGroup>
+          <Label for="nom">Nom de l'artiste</Label>
+          <Input type="text" name="Nom" id="NomArtiste"  />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Date">Date de Naissance</Label>
+          <Input
+            type="date"
+            name="Naissance"
+            id="DatedeNaissance"
+            
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Followers">Followers</Label>
+          <Input
+            type="number"
+            name="followers"
+            id="nbrfollowers"
+           
+          />
+        </FormGroup>
+       
+        <FormGroup>
+          <Label for="Albums">Albums</Label>
+          <Input type="textarea" name="album" id="albums" />
+        </FormGroup>
+        <Button className="Submit">Submit</Button>
+      </Form>
+      </Col>
+
+      <Col md="4">
+      <Form className = "NewAlbum" onSubmit={this.handleSubmit}>
+
+      <h1>Ajouter un Album</h1>
+
+      
+        <FormGroup>
+          <Label for="titre">Titre de l'album</Label>
+          <Input type="text" name="Titre" id="TitreAlbum"  />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Date">Date de sortie</Label>
+          <Input
+            type="date"
+            name="Sortie"
+            id="DatedeSortie"
+            
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="genre">Genre</Label>
+          <Input type="text" name="Genre" id="GenreAlbum"  />
+        </FormGroup>
+        <FormGroup>
+          <Label for="urlcover">URL Cover</Label>
+          <Input type="text" name="UrlCover" id="UrlCoverAlbum"  />
+        </FormGroup>
+        <FormGroup>
+          <Label for="reftitres">Ref Titres</Label>
+          <Input type="text" name="RefTitres" id="RefTitresAlbum"  />
+        </FormGroup>
+        
+        <Button className="Submit">Submit</Button>
+      </Form>
+      </Col>
+
+
+      <Col md="4">
+      <Form className = "NewTitre" onSubmit={this.handleSubmit}>
+
+      <h1>Ajouter un Titre</h1>
+
+      
+        <FormGroup>
+          <Label for="titrechanson">Titre</Label>
+          <Input type="text" name="Titrechanson" id="TitreChanson"  />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="duree">Durée</Label>
+          <Input
+            type="number"
+            name="dureechanson"
+            id="DureeChanson"
+           
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Ecoutes">Ecoutes</Label>
+          <Input
+            type="number"
+            name="ecouteschanson"
+            id="EcoutesChanson"
+           
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="Followers">Mention "j'aime"</Label>
+          <Input
+            type="number"
+            name="nbrjaime"
+            id="NbrJaime"
+           
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="refartistefeat">Featuring</Label>
+          <Input type="text" name="Refartistefeat" id="RefArtisteFeat"  />
+        </FormGroup>
+        
+        <Button className="Submit">Submit</Button>
+      </Form>
+      </Col>
+
+      </Row>
+      </Form>
+    );
+  }
+}
+ 
+ 
