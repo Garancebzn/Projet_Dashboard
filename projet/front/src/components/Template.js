ba9eed2ec796bdf0478906ea17d9a68e61d00302
Roxane <roxane.batigny@edu.ece.fr>
2019-04-04 18:11:52 +0200

    reparation front

--- projet/front/src/components/Template.js
+++ projet/front/src/components/Template.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 import {
